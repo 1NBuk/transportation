bb93e3f9d21d3eea565a891784d923a50cb47463
mriii31 <mariielysen@gmail.com>
2024-11-21 19:42:45 +0300

    search for user (NO DATE YET)

--- src/main/java/org/example/transportation/controller/BlogUserController.java
+++ src/main/java/org/example/transportation/controller/BlogUserController.java
@@ -1,11 +1,17 @@
 package org.example.transportation.controller;
 
+import org.example.transportation.entity.BlogPost;
 import org.example.transportation.service.BlogPostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user_blog")
@@ -23,5 +29,17 @@ public class BlogUserController {
     @GetMapping("/")
     public String homePage() {
         return "index_user"; // Убедитесь, что шаблон index.html существует
+    }
+
+    @GetMapping("/search")
+    public String searchBlogPosts(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String content,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            Model model) {
+
+        List<BlogPost> searchResults = blogPostService.searchBlogPosts(title, content, date);
+        model.addAttribute("blogPosts", searchResults);
+        return "user_blog";
     }
 }
